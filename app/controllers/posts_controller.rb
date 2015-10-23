da2336f87e2b766368927d6efb6b0d641f9d51ef
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-10-23 15:32:48 -0400

    finished functionality for editing users, now working on crud for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,38 @@ class PostsController < ApplicationController
   	@post = Post.find(params[:id])
   end
 
-  def update   
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update  
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end 
   end 
   # def delete
   # 	@post = Post.find(params[:id])
@@ -28,4 +59,15 @@ class PostsController < ApplicationController
     redirect_to :back
   end
 
+  # private
+  # # Use callbacks to share common setup or constraints between actions.
+  #   def set_post
+  #     @post = Post.find(params[:id])
+  #   end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+      params.require(:post).permit(:fname, :lname, :email, :password)
+    end
+end 
 end
